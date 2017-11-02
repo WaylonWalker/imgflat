f334368d29d0c7750652cac81fb17d52c80dc7f5
Walker Waylon Scott <walker_waylon_s@cat.com>
2017-11-01 23:50:51 -0500

    added recursive feature

--- imgflat/inline_img.py
+++ imgflat/inline_img.py
@@ -9,23 +9,26 @@ env = Environment(
     autoescape=select_autoescape(['html', '.md']),
 )
 
-@click.command(help='hi')
+@click.command(help='A tool for creating a flat file containing all of the images in a directory inline.')
 @click.option('--output',
     default='default.md',
     help='output filename and extension')
+@click.option('--recursive',
+              is_flag=True,
+              default=False,
+              help='Recurse through file system or use top level directory only')
 def inline(
     output,
+    recursive,
     img_types=['*.png', '*.jpg'],
 ):
     
     basename = os.path.basename(os.getcwd())
     
     if output == 'default.md':
-        output = f'{basename}.md'
+        output = f'images in {basename}.md'
 
     output_type = os.path.splitext(output)[1]
-    print(f'{output}: type;{output_type}')
-
 
     if output_type in ['.markdown', '.md', '.multimarkdown', '.multi-markdown']:
         template = env.get_template('markdown.md')
@@ -34,9 +37,12 @@ def inline(
     else:
         click.echo(f'output type {output_type} not supported')
 
+    if recursive:
+        img_types = [f'**/*{img_type}' for img_type in img_types]
+
     files = []
     for type in img_types:
-        files.extend(glob.glob(type))
+        files.extend(glob.glob(type, recursive=recursive))
 
     encoded = {}
     for file in files:
